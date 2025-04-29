eb981334e6d53fe4be789a812c18cc4817cf290b
VosMovok <dimasnugraha46@gmail.com>
2025-04-29 12:45:35 +0800

    creating register path

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,8 +6,10 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\LogoutRequest;
 use App\Http\Resources\MessageResource;
 use App\Http\Resources\UserResource;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
@@ -83,13 +85,12 @@ class AuthController
             $user = JWTAuth::parseToken()->authenticate();
 
             if (!$user) {
-                return (new MessageResource(null, false, 'User not found'))->response()
-                    ->header('X-User-ID', $user->id)
-                    ->header('X-User-Email', $user->email)
-                    ->setStatusCode(401);
+                return (new MessageResource(null, false, 'User not found'))->response()->setStatusCode(401);
             }
 
-            return (new MessageResource(new UserResource($user), true, 'Token verified'))->response()->setStatusCode(200);
+            return (new MessageResource(null, true, 'Token verified'))->response()
+                ->header('X-User-ID', $user->id)
+                ->setStatusCode(200);
         } catch (JWTException $e) {
             return (new MessageResource(null, false, 'Could not verify token', $e->getMessage()))->response()->setStatusCode(401);
         }
@@ -116,5 +117,53 @@ class AuthController
         }
 
         return (new MessageResource(null, true, 'Logout successful'))->response()->setStatusCode(200);
+    }
+
+    /**
+     * Handle user register
+     */
+    public function register(LoginRequest $request): JsonResponse
+    {
+        if (isset($request->validator) && $request->validator->fails()) {
+            return (new MessageResource(null, false, 'Validation failed', $request->validator->messages()))->response()->setStatusCode(400);
+        }
+
+        $validatedData = $request->validated();
+
+        try {
+            $response = Http::post("http://user-service.default.svc.cluster.local:8000/api/users", [
+                'email' => $validatedData['email'],
+                'password' => $validatedData['password'],
+            ]);
+
+            error_log("User Service Response: " . $response->body());
+            if (!$token = JWTAuth::attempt($validatedData)) {
+                return (new MessageResource(null, false, 'Invalid Credentials'))->response()->setStatusCode(401);
+            }
+
+            $user = JWTAuth::claims(['user' => Auth::user()])->user();
+
+            // create refresh token
+            JWTAuth::setToken($token);
+            $refreshToken = JWTAuth::refresh();
+
+            // combine user and token to json
+            $combined = [
+                'user' => new UserResource($user),
+                'access_token' => $token,
+                'refresh_token' => $refreshToken,
+                'expires_in' => config('jwt.ttl'),
+                'token_type' => 'Bearer',
+            ];
+
+            return (new MessageResource($combined, true, 'Register Success'))->response()->setStatusCode(200);
+        } catch (JWTException $e) {
+            return (new MessageResource(null, false, 'Could not create token', $e->getMessage()))->response()->setStatusCode(500);
+        } catch (RequestException $e) {
+            return (new MessageResource(null, false, 'Could not create user', $e->getMessage()))->response()->setStatusCode(500);
+        } catch (\Exception $e) {
+            return (new MessageResource(null, false, 'Could not create user', $e->getMessage()))->response()->setStatusCode(500);
+        }
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::get('refresh', [AuthController::class, 'refresh']);
     Route::get('verify', [AuthController::class, 'verify']);
+    Route::post('register', [AuthController::class, 'register']);
 });
